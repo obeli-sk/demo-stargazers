72085add268bac12dd574975719cfd0b07ec5da0
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-12-31 12:08:26 +0100

    feat: Implement the chatgpt activity

---
+++ llm/chatgpt/src/lib.rs
@@ -0,0 +1,122 @@
+use exports::stargazers::llm::llm::Guest;
+use serde::{Deserialize, Serialize};
+use std::env;
+use waki::Client;
+use wit_bindgen::generate;
+
+generate!({ generate_all });
+pub(crate) struct Component;
+export!(Component);
+
+#[derive(Serialize)]
+struct ChatGPTRequest {
+    model: String,
+    messages: Vec<Message>,
+    max_tokens: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Role {
+    User,
+    Assistant,
+    System,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    role: Role,
+    content: String,
+}
+
+#[derive(Deserialize)]
+struct ChatGPTResponse {
+    choices: Vec<Choice>,
+}
+
+#[derive(Deserialize)]
+struct Choice {
+    message: MessageResponse,
+}
+
+#[derive(Deserialize)]
+struct MessageResponse {
+    content: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Settings {
+    model: String,
+    messages: Vec<Message>,
+    max_tokens: usize,
+}
+
+impl Guest for Component {
+    fn respond(user_prompt: String, settings: String) -> Result<String, String> {
+        let api_key = env::var("OPENAI_API_KEY")
+            .expect("OPENAI_API_KEY must be set as an environment variable");
+
+        let settings: Settings =
+            serde_json::from_str(&settings).expect("`settings_json` must be parseable");
+
+        let mut messages = settings.messages;
+        messages.push(Message {
+            role: Role::User,
+            content: user_prompt,
+        });
+
+        let request_body = ChatGPTRequest {
+            model: settings.model,
+            messages,
+            max_tokens: settings.max_tokens,
+        };
+
+        let resp = Client::new()
+            .post("https://api.openai.com/v1/chat/completions")
+            .header("Authorization", format!("Bearer {}", api_key))
+            .header("Content-Type", "application/json")
+            .json(&request_body)
+            .send()
+            .map_err(|err| format!("{:?}", err))?;
+
+        if resp.status_code() != 200 {
+            return Err(format!("Unexpected status code: {}", resp.status_code()));
+        }
+
+        let response: ChatGPTResponse = resp.json().map_err(|err| format!("{:?}", err))?;
+
+        if let Some(choice) = response.choices.first() {
+            Ok(choice.message.content.clone())
+        } else {
+            Err("No response from ChatGPT".to_string())
+        }
+    }
+}
+
+mod tests {
+
+    #[test]
+    #[ignore]
+    fn request_should_succeed() {
+        use crate::exports::stargazers::llm::llm::Guest;
+        use crate::Component;
+        use crate::{Message, Role, Settings};
+
+        let user_prompt = std::env::var("CHATGPT_USER_PROMPT")
+            .unwrap_or_else(|_| "Tell me about Rust programming.".to_string());
+        let settings_json = std::env::var("CHATGPT_SETTINGS_JSON").unwrap_or_else(|_| {
+            serde_json::to_string(&Settings {
+                messages: vec![Message {
+                    role: Role::System,
+                    content: "You are a helpful assistant".to_string(),
+                }],
+                model: "gpt-3.5-turbo".to_string(),
+                max_tokens: 50,
+            })
+            .unwrap()
+        });
+        let res = Component::respond(user_prompt, settings_json);
+        let res = res.unwrap();
+        println!("{res}");
+    }
+}
--- workflow/src/lib.rs
+++ workflow/src/lib.rs
@@ -1,12 +1,5 @@
 use crate::exports::stargazers::workflow::workflow::Guest;
-use stargazers::{
-    account::account,
-    db::{
-        prompt,
-        user::{self},
-    },
-    llm::llm,
-};
+use stargazers::{account::account, db, llm::llm};
 use wit_bindgen::generate;
 
 generate!({ generate_all });
@@ -16,20 +9,20 @@ export!(Component);
 impl Guest for Component {
     fn star_added(sender: String, repo: String) -> Result<(), String> {
         // Persist the user giving a star to the project.
-        let description = user::link(&sender, &repo)?;
+        let description = db::user::link(&sender, &repo)?;
         if description.is_none() {
             // Fetch the account info from github.
+            // TODO: account::info and get_settings_json should run in parallel
             let info = account::info(&sender)?;
-            let prompt = prompt::get()?;
-            let prompt = format!("{prompt}\n{info}");
+            let settings_json = db::llm::get_settings_json()?;
             // Generate the user's description.
-            let description = llm::respond(&prompt)?;
-            user::user_update(&sender, &description)?;
+            let description = llm::respond(&info, &settings_json)?;
+            db::user::user_update(&sender, &description)?;
         }
         Ok(())
     }
 
     fn star_removed(sender: String, repo: String) -> Result<(), String> {
-        user::unlink(&sender, &repo)
+        db::user::unlink(&sender, &repo)
     }
 }
