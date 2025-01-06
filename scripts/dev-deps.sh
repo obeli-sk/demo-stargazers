a88fdc1192da507b753a3c3bfb79828ee4337f1f
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-01-06 23:48:51 +0100

    build: Add `dev-deps`

---
+++ scripts/dev-deps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Collect versions of binaries installed by `nix develop` producing file `dev-deps.txt`.
+# This script should be executed after every `nix flake update`.
+
+set -exuo pipefail
+cd "$(dirname "$0")/.."
+
+rm -f dev-deps.txt
+echo "cargo-binstall $(cargo-binstall -V)" >> dev-deps.txt
+cargo upgrade --version >> dev-deps.txt
+cargo-expand --version >> dev-deps.txt
+cloudflared --version >> dev-deps.txt
+echo "pkg-config $(pkg-config --version)" >> dev-deps.txt
+rustc --version >> dev-deps.txt
+wasm-tools --version >> dev-deps.txt
+wasmtime --version >> dev-deps.txt
+# libc
+ldd --version | head -n 1 >> dev-deps.txt
