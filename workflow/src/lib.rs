e04e37c65570136fac92c75d53b3a724335ac878
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-01-19 20:45:21 +0100

    feat: Enable parallelism in `star-added`
    If the description was not generated yet, create a join set for each
    of the following activities:
    - `account-info`
    - `get-settings-json`
    Both activities are submitted for execution, then the workflow waits until
    both of them finish using the `-await-next` extension.

--- workflow/src/lib.rs
+++ workflow/src/lib.rs
@@ -1,9 +1,6 @@
 use crate::exports::stargazers::workflow::workflow::Guest;
 use stargazers::{
-    account::account::{self, list_stargazers},
-    db,
-    llm::llm,
-    workflow::workflow as imported_workflow,
+    account::account::list_stargazers, db, llm::llm, workflow::workflow as imported_workflow,
 };
 use wit_bindgen::generate;
 
@@ -16,11 +13,25 @@ impl Guest for Component {
         // Persist the user giving a star to the project.
         let description = db::user::link_get_description(&login, &repo)?;
         if description.is_none() {
-            // Fetch the account info from github.
-            // TODO: account_info and get_settings_json should run in parallel
-            let info = account::account_info(&login)?;
-            // TODO: cache for 5 mins
-            let settings_json = db::llm::get_settings_json()?;
+            // Parallel fetch account_info and get_settings_json
+            let join_set_info = obelisk::workflow::workflow_support::new_join_set();
+            let join_set_settings = obelisk::workflow::workflow_support::new_join_set();
+
+            stargazers::account_obelisk_ext::account::account_info_submit(&join_set_info, &login);
+            stargazers::db_obelisk_ext::llm::get_settings_json_submit(&join_set_settings);
+
+            let (_, info_result) =
+                stargazers::account_obelisk_ext::account::account_info_await_next(&join_set_info)
+                    .map_err(|(_, e)| format!("{:?}", e))?;
+
+            let info = info_result?;
+
+            let (_, settings_result) =
+                stargazers::db_obelisk_ext::llm::get_settings_json_await_next(&join_set_settings)
+                    .map_err(|(_, e)| format!("{:?}", e))?;
+
+            let settings_json = settings_result?;
+
             // Generate the user's description.
             let description = llm::respond(&info, &settings_json)?;
             db::user::user_update(&login, &description)?;
