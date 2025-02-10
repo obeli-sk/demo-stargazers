e2ccd626fd951109a042efa0fdea52dc5a12b12c
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-02-10 13:45:41 +0100

    chore: Update to Obelisk 0.15.0

--- workflow/src/lib.rs
+++ workflow/src/lib.rs
@@ -1,7 +1,7 @@
 use crate::exports::stargazers::workflow::workflow::Guest;
 use obelisk::{
     types::execution::{ExecutionError, ExecutionId},
-    workflow::workflow_support::new_join_set,
+    workflow::workflow_support::new_join_set_named,
 };
 use stargazers::{
     db,
@@ -40,8 +40,8 @@ impl Guest for Component {
         let description = db::user::add_star_get_description(&login, &repo)?;
         if description.is_none() {
             // Create two join sets for the two child workflows.
-            let join_set_info = new_join_set(&format!("info_{login}"));
-            let join_set_settings = new_join_set(&format!("settings_{login}"));
+            let join_set_info = new_join_set_named(&format!("info_{login}"));
+            let join_set_settings = new_join_set_named(&format!("settings_{login}"));
             // Submit the two child workflows asynchronously.
             account_info_submit(&join_set_info, &login);
             get_settings_json_submit(&join_set_settings);
@@ -92,7 +92,7 @@ impl Guest for Component {
                 // No need to await the result of the child workflow.
                 // When this execution completes, all join sets will be awaited.
                 imported_workflow_ext::star_added_parallel_submit(
-                    &new_join_set(login),
+                    &new_join_set_named(login),
                     login,
                     &repo,
                 );
