48a3625d22fbc7a5d316c0ea65411e8026c7b1f2
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-05-12 11:07:29 +0200

    refactor(go): Make a simple GET request with `wasi-go-sdk`

--- activity/llm/openai-go/main.go
+++ activity/llm/openai-go/main.go
@@ -1,22 +1,55 @@
 package main
 
-import "fmt"
-import "go.bytecodealliance.org/cm"
-import logbindings "github.com/obeli-sk/demo-stargazers/openai-go/gen/obelisk/log/log"
-import llmbindings "github.com/obeli-sk/demo-stargazers/openai-go/gen/stargazers/llm/llm"
+import (
+	"fmt"
+	"io"
+	"net/http"
+"go.bytecodealliance.org/cm"
+"github.com/rajatjindal/wasi-go-sdk/pkg/wasihttp"
+logbindings "github.com/obeli-sk/demo-stargazers/openai-go/gen/obelisk/log/log"
+llmbindings "github.com/obeli-sk/demo-stargazers/openai-go/gen/stargazers/llm/llm"
+"os"
+)
 
 
-func myHardcodedLLMResponse(userPrompt string, settingsJSON string) (result cm.Result[string, string, string]) {
-	fmt.Printf("[WASM Guest llm.Respond] Received prompt: \"%s\"\n", userPrompt)
-	fmt.Printf("[WASM Guest llm.Respond] Received settings: \"%s\"\n", settingsJSON)
-	logbindings.Trace("Hello")
-	hardcodedResponse := "This is a hardcoded response from the Wasm module. Prompt and settings were received."
-	return cm.OK[cm.Result[string, string, string], string /* Shape */, string /* OK */, string /* Err */](hardcodedResponse)
+func respond(userPrompt string, settingsJSON string) (result cm.Result[string, string, string]) {
+	value := os.Getenv("OPENAI_API_KEY")
+	fmt.Println("OPENAI_API_KEY:", value)
+	
+	client := wasihttp.NewClient()
+	req, err := http.NewRequest(http.MethodGet, userPrompt, nil)
+	if err != nil {
+		errorMsg := fmt.Sprintf("failed to create request: %v", err)
+		return cm.Err[cm.Result[string, string, string]](errorMsg)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		errorMsg := fmt.Sprintf("failed to make outbound request: %v", err)
+		return cm.Err[cm.Result[string, string, string]](errorMsg)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		errorMsg := fmt.Sprintf("expected status code: %d, got: %d", http.StatusOK, resp.StatusCode)
+		return cm.Err[cm.Result[string, string, string]](errorMsg)
+	}
+
+	raw, err := io.ReadAll(resp.Body)
+	if err != nil {
+		errorMsg := fmt.Sprintf("failed to read response body: %v", err)
+		return cm.Err[cm.Result[string, string, string]](errorMsg)
+	}
+
+
+	logbindings.Debug("HTTP request succeeded")
+	
+	return cm.OK[cm.Result[string, string, string]](string(raw))
 }
 
 func init() {
 	// stargazers:llm/llm.respond
-	llmbindings.Exports.Respond = myHardcodedLLMResponse
+	llmbindings.Exports.Respond = respond
 }
 
 func main() {
