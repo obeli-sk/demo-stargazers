c05bc576ea0f8208583ca25e08e96d994e9248bd
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-05-16 18:57:27 +0200

    chore: Add scripts for bumping Go,JS dependencies

--- scripts/bump-deps-go.sh
+++ scripts/bump-deps-go.sh
@@ -3,12 +3,12 @@
 set -exuo pipefail
 cd "$(dirname "$0")/.."
 
-
 find . -name 'go.mod' | while read modfile; do
   dir=$(dirname "$modfile")
-  echo "Building $dir"
+  echo "Updating deps in $dir"
   (
     cd "$dir"
-    ./build.sh
+    go get -u ./...
+    go mod tidy
   )
 done
--- scripts/bump-deps-js.sh
+++ scripts/bump-deps-js.sh
@@ -5,10 +5,9 @@ cd "$(dirname "$0")/.."
 
 find . -name 'package.json' -not -path '*/node_modules/*' | while read modfile; do
   dir=$(dirname "$modfile")
-  echo "Building $dir"
+  echo "Updating deps in $dir"
   (
     cd "$dir"
-    npm install
-    npm run build
+    npm update
   )
 done
