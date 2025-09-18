e31fadf35061d74cfda7d68a5a0f8299bbf5dac6
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-09-18 12:22:00 +0200

    chore: Move generated extensions to `gen` folder

--- scripts/obelisk-generate-extensions.sh
+++ scripts/obelisk-generate-extensions.sh
@@ -7,14 +7,19 @@ generate() {
   local path="$1"
   local component_type="$2"
 
+  if [ ! -d "$path" ]; then
+    return 0
+  fi
   find "$path" -maxdepth 1 -type d -exec test -d "{}/wit" \; -print | while read -r dir; do
     echo "Updating $dir"
     (
-      cd "$dir" || exit
-      obelisk generate extensions "$component_type" wit
+      cd "$dir/wit"
+      rm -rf gen
+      obelisk generate extensions "$component_type" . gen
     )
   done
 }
 
 generate "activity" "activity_wasm"
+generate "activity-stub" "activity_stub"
 generate "workflow" "workflow"
