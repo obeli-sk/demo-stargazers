1ab352ebcfb42b01f9265bc75e07bce2629022b1
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-02-09 22:13:12 +0100

    build: Replace `sed` with `obelisk component add`

--- scripts/push-components.sh
+++ scripts/push-components.sh
@@ -10,23 +10,25 @@ TOML_FILE="obelisk-oci.toml"
 PREFIX="docker.io/getobelisk/demo_stargazers_"
 
 push() {
-    RELATIVE_PATH=$1
+    COMPONENT_TYPE=$1
+    RELATIVE_PATH=$2
+    
     FILE_NAME_WITHOUT_EXT=$(basename "$RELATIVE_PATH" | sed 's/\.[^.]*$//')
     OCI_LOCATION="${PREFIX}${FILE_NAME_WITHOUT_EXT}:${TAG}"
     echo "Pushing ${RELATIVE_PATH} to ${OCI_LOCATION}..."
     OUTPUT=$(obelisk component push "$RELATIVE_PATH" "$OCI_LOCATION")
 
     # Replace the old location with the actual OCI location
-    sed -i -E "/name = \"${FILE_NAME_WITHOUT_EXT}\"/{n;s|location = \".*\"|location = \"${OUTPUT}\"|}" "$TOML_FILE"
+    obelisk component add ${COMPONENT_TYPE} ${OUTPUT} --name ${FILE_NAME_WITHOUT_EXT} -c $TOML_FILE
 }
 
 # Rebuild rust components
 just rust
 
-push "target/wasm32-wasip2/release/activity_llm_openai.wasm"
-push "target/wasm32-wasip2/release/activity_github_impl.wasm"
-push "target/wasm32-wasip2/release/activity_db_turso.wasm"
-push "target/wasm32-unknown-unknown/release/workflow.wasm"
-push "target/wasm32-wasip2/release/webhook.wasm"
+push activity_wasm "target/wasm32-wasip2/release/activity_llm_openai.wasm"
+push activity_wasm "target/wasm32-wasip2/release/activity_github_impl.wasm"
+push activity_wasm "target/wasm32-wasip2/release/activity_db_turso.wasm"
+push workflow "target/wasm32-unknown-unknown/release/workflow.wasm"
+push webhook_endpoint "target/wasm32-wasip2/release/webhook.wasm"
 
 echo "All components pushed and TOML file updated successfully."
